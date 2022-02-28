1feba7e152f49fe42de7529e967294e5fe721b4b
MilonMridha <milonmridha570@gmail.com>
2022-03-01 00:41:02 +0600

    load Api

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log("Milon");
+// Load & Fetch All Phones Data------------------>
+const loadPhoneData = () => {
+  const inputField = document.getElementById("input-field");
+  const inputText = inputField.value;
+  inputField.value = "";
+  if (inputText == "" || !isNaN(inputText)) {
+    alert("Enter Phone Name");
+  } else {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayPhones(data.data.slice(0, 20)));
+  }
+};
